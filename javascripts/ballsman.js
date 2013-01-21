f96ee044c1d9f26fb6cbd8aeb4a44afc1449def8
m2 <max21906@gmail.com>
2013-01-21 23:15:57 +0100

    init ballsman
    next step: terrain from heightmap

--- javascripts/ballsman.js
+++ javascripts/ballsman.js
@@ -1,4 +1,25 @@
-﻿$(function () {
+﻿
+
+/************
+ * BMCORE
+ ***********/
+
+bmcore = {
+    init: function (opt) {
+
+        $.extend(this, opt);
+
+        return this;
+    },
+
+    update: function (t) {
+    }
+};
+
+
+
+
+$(function () {
 
     // get the DOM element to attach to
     // - assume we've got jQuery to hand
@@ -9,26 +30,40 @@
         HEIGHT = WIDTH * (9 / 16);
 
     // set some camera attributes
-    var VIEW_ANGLE = 45,
+    var FOV = 60,
         ASPECT = WIDTH / HEIGHT,
         NEAR = 10,
-        FAR = 1500;
+        FAR = 1500,
 
-    /* STATS */
+        SPHERERADIUS = 250;
 
-    stats = new Stats();
-    $("#header").before(stats.domElement);
+    /* statsPanel */
+
+    statsPanel = new Stats();
+    $("#title").before(statsPanel.domElement);
+
+    bmcore.init({
+        sphereradius: SPHERERADIUS
+    });
 
 
-    /* RENDERER */
+    /************
+     * RENDERER
+     ***********/
 
     // create a WebGL renderer, camera
     // and a scene
     var renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.shadowMapEnabled = true;
-    renderer.shadowMapType = THREE.PCFShadowMap;
-    // to antialias the shadow
-    renderer.shadowMapSoft = true;
+
+    var UseFloatTextures = renderer.context.getExtension('OES_texture_float') != null;
+
+    // enable shadowmap
+    //if (UseFloatTextures) {
+    //    renderer.shadowMapEnabled = true;
+    //    renderer.shadowMapType = THREE.PCFShadowMap;
+    //    // to antialias the shadow
+    //    renderer.shadowMapSoft = true;
+    //}
 
     // start the renderer
     renderer.setSize(WIDTH, HEIGHT);
@@ -37,21 +72,36 @@
     $container.append(renderer.domElement);
 
 
-    /* CAMERA */
-    var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
 
-    camera.position.z = 1700;
+    /************
+     * SCENE
+     ***********/
 
-
-    /* SCENE */
     var scene = new THREE.Scene();
-    scene.fog = new THREE.Fog(0x000000, 750, 1500);
+    //scene.fog = new THREE.Fog(0x000000, 750, 1500);
 
 
-    /* CONTROLS */
+
+
+    /************
+     * CAMERA
+     ***********/
+
+    var camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
+
+    camera.position.z = bmcore.sphereradius * 2;
+
+    scene.add(camera);
+
+
+
+
+    /************
+     * CONTROLS
+     ***********/
 
     // init the mouse control
-    
+
     controls = new THREE.TrackballControls(camera, $container[0]);
     controls.target.set(0, 0, 0);
 
@@ -66,7 +116,19 @@
     controls.dynamicDampingFactor = 0.15;
 
     controls.keys = [65, 83, 68];
+
+    controls.enabled = true;
+
+    // init the control panel
+
+    controlPanel = new dat.GUI({ autoPlace: false });
+    $("#title").before(controlPanel.domElement);
     
+    controlPanel.addFolder("Mouse control")
+        .add(controls, 'enabled');
+    //controlPanel.closed = true;
+
+
 
 
     /************
@@ -75,20 +137,21 @@
 
     // create the sphere's material
     var shininess = 200,
-        sphereColors = [0xCC1111, 0xCCCC11, 0x11CC11],
-        sphereMaterials = []
+        sphereColors = [0xFFFFFF, 0x11CC11]
     ;
 
-    for (var i = 0; i < 3; i++) {
-        var sphereMaterial = new THREE.MeshPhongMaterial(
-        {
-            color: sphereColors[i],
-            transparent: true,
-            specular: 0x555555, shininess: shininess
-        });
+    var matOpt = {
+        color: sphereColors[0],
+        transparent: true,
+        specular: sphereColors[0], shininess: shininess,
 
-        sphereMaterials.push(sphereMaterial);
-    }
+        wireframe: true
+    };
+
+    sphereMaterial = UseFloatTextures ?
+        new THREE.MeshPhongMaterial(matOpt) :
+        new THREE.MeshLambertMaterial(matOpt)
+    ;
 
 
     var planeTexture = THREE.ImageUtils.loadTexture("images/plane.png");
@@ -96,11 +159,64 @@
     planeTexture.repeat.set(25, 25);
 
     var planeMaterial = new THREE.MeshLambertMaterial(
+    //var planeMaterial = new THREE.MeshNormalMaterial(
     {
         name: "planeMaterial",
-        color: 0xCCCCCC,
-        map: planeTexture
+        map: planeTexture,
+        transparent: true,
+        opacity: 0.25,
+        color: 0xCECECE,
+        side: THREE.DoubleSide
     });
+
+
+
+
+    /************
+     * LIGHTS
+     ***********/
+
+    /* ambient */
+    var ambientLight = new THREE.AmbientLight(0x222222);
+
+    /* spot */
+    var spotLight = new THREE.SpotLight(0x444444);
+
+    //spotLight.castShadow = true;
+
+    //spotLight.shadowCameraNear = 600;
+    //spotLight.shadowCameraFar = 1500;
+    //spotLight.shadowCameraFov = 120;
+    //spotLight.shadowCameraVisible = true;
+    //spotLight.shadowMapWidth = spotLight.shadowMapHeight = 2048;
+    //spotLight.shadowDarkness = 0.1;
+
+    // set its position
+    spotLight.position.x = 500;
+    spotLight.position.y = 500;
+    spotLight.position.z = 500;
+
+
+    /* spot */
+    var spotLight2 = new THREE.SpotLight(0x444444);
+
+    //spotLight2.shadowCameraNear = 600;
+    //spotLight2.shadowCameraFar = 1500;
+    //spotLight2.shadowCameraFov = 120;
+    //spotLight2.shadowCameraVisible = true;
+    //spotLight2.shadowMapWidth = spotLight2.shadowMapHeight = 756;
+    //spotLight2.shadowDarkness = 0.1;
+
+    // set its position
+    spotLight2.position.x = -500;
+    spotLight2.position.y = -500;
+    spotLight2.position.z = 500;
+
+
+
+    scene.add(ambientLight);
+    scene.add(spotLight);
+    scene.add(spotLight2);
 
 
 
@@ -111,7 +227,7 @@
 
     /** Plane **/
 
-    var plane0 = function initPlane() {
+    var plane = function initPlane() {
         var width = 2000,
             height = width,
             wSegments = 50,
@@ -128,18 +244,15 @@
         return plane;
     }();
 
-    //plane0.rotation.x = -Math.PI/2;
-
-    // add the plane to the scene
-    scene.add(plane0);
+    //plane.rotation.x = -Math.PI/2;
 
 
-    /** Spheres **/
+    /** Sphere **/
 
-    var spheres = function initSpheres() {
-        var radius = 50,
-            segments = 32,
-            rings = segments
+    var sphere = function initSphere() {
+        var radius = SPHERERADIUS,
+            segments = 64,
+            rings = segments/2
         ;
 
         var arr = [];
@@ -148,147 +261,52 @@
         // 0
         sphere = new THREE.Mesh(
             new THREE.SphereGeometry(radius, segments, rings),
-            sphereMaterials[0]
+            sphereMaterial
         );
         sphere.name = "sphere0";
         sphere.castShadow = true;
         sphere.receiveShadow = false;
 
-        sphere.position.x = -150;
-        sphere.position.z = radius;
 
-        arr.push(sphere);
-
-        // 1
-        sphere = new THREE.Mesh(
-            new THREE.SphereGeometry(radius, segments, rings),
-            sphereMaterials[1]
-        );
-        sphere.name = "sphere1";
-        sphere.castShadow = true;
-        sphere.receiveShadow = false;
-
-        sphere.position.x = 0;
-        sphere.position.z = radius;
-
-        arr.push(sphere);
-
-        // 2
-        sphere = new THREE.Mesh(
-            new THREE.SphereGeometry(radius, segments, rings),
-            sphereMaterials[2]
-        );
-        sphere.name = "sphere2";
-        sphere.castShadow = true;
-        sphere.receiveShadow = false;
-
-        sphere.position.x = 150;
-        sphere.position.z = radius;
-
-        arr.push(sphere);
-
-        return arr;
+        return sphere;
     }();
 
-    // add the spheres to the scene
-    spheres.forEach(function (o, i) { scene.add(o); })
 
+    var siz = 150, res = 5,
+        cubeGeometry = new THREE.CubeGeometry(
+        siz, siz, siz,
+        res, res, res
+    );
+    for (var i = 0; i < cubeGeometry.faces.length; i++) {
 
+        cubeGeometry.faces[i].color.setHex(Math.random() * 0xffffff);
 
-    /************
-     * CAMERA
-     ***********/
+    }
 
-    // and the camera
-    scene.add(camera);
+    cube = new THREE.Mesh(
+        cubeGeometry,
+        new THREE.MeshBasicMaterial({ vertexColors: THREE.FaceColors })
+    );
 
+    scene.add(cube);
+    scene.add(plane);
+    scene.add(sphere);
 
-
-    /************
-     * LIGHTS
-     ***********/
-
-    // ambient
-    var ambientLight = new THREE.AmbientLight(0x222222);
-    scene.add(ambientLight);
-
-    // create a point light
-    var spotLight = new THREE.SpotLight(0xEEEEEE);
-
-    spotLight.castShadow = true;
-
-    spotLight.shadowCameraNear = 600;
-    spotLight.shadowCameraFar = 1500;
-    spotLight.shadowCameraFov = 120;
-
-    //spotLight.shadowCameraVisible = true;
-
-    spotLight.shadowMapWidth = spotLight.shadowMapHeight = 2048;
-
-    spotLight.shadowDarkness = 0.1;
-
-    // set its position
-    spotLight.position.x = -150;
-    spotLight.position.y = 500;
-    spotLight.position.z = 500;
-
-    scene.add(spotLight);
-
-    // create a point light
-    var spotLight2 = new THREE.SpotLight(0x555555);
-
-    spotLight2.shadowCameraNear = 600;
-    spotLight2.shadowCameraFar = 1500;
-    spotLight2.shadowCameraFov = 120;
-
-    //spotLight2.shadowCameraVisible = true;
-
-    spotLight2.shadowMapWidth = spotLight2.shadowMapHeight = 756;
-
-    spotLight2.shadowDarkness = 0.1;
-
-    // set its position
-    spotLight2.position.x = 0;
-    spotLight2.position.y = -500;
-    spotLight2.position.z = 1;
-
-    scene.add(spotLight2);
-
-
-
-    sphereLights = [];
-    spheres.forEach(function (o, i) {
-        var light = new THREE.PointLight(o.material.color.getHex());
-        light.position.x = o.position.x;
-        light.position.y = o.position.y;
-        light.position.z = o.position.z;
-        light.intensity = 0;
-
-        sphereLights.push(light);
-
-        scene.add(light);
-    });
-
-
-
-    // init my objects
 
     function update(time) {
 
         window.requestAnimationFrame(update);
 
-        //controls.update();
+        bmcore.update(time);
+
+        controls.update();
 
         //spotLight.position.x = Math.cos(time / 900) * 50 - 25;
         //spotLight.position.y = Math.cos(time / 900) * 50 - 25 + 600;
 
-        //spherespotLight.position = spotLight.position;
-
-        stats.update();
+        statsPanel.update();
         renderer.render(scene, camera);
     };
-
-    ledState = [false, false, false];
 
     // draw!
     update();
@@ -329,9 +347,9 @@
                 h = $container.height()
             ;
 
-            return { 
+            return {
                 x: (x / w) * 2 - 1,
-                y: -(y / h) * 2 + 1 
+                y: -(y / h) * 2 + 1
             };
         }();
 
@@ -352,44 +370,12 @@
                 id = o.name.charAt(o.name.length - 1) * 1
             ;
 
-            if (ledState[id] == true) {
-                ledState[id] = false;
-                sphereLights[id].intensity = .1;
-                o.material.emissive = new THREE.Color(0);
-            } else {
-                ledState[id] = true;
-                sphereLights[id].intensity = .8;
-                o.material.emissive = o.material.color;
-            }
+            console.log("Click on sphere (%s)", id);
         }
 
     }, false);
 
-    __marg = 1500 - 900;
-    camera.position.z = 900;
-    animation.animate({
-        duration: 4000,
-        delta: animation.makeEaseOut(animation.elastic),
-        step: function (d) {
-            camera.position.z = 1500 - __marg * d;
-        }
-    });
-    //animation.animate({
-    //    duration: 3500,
-    //    delta: animation.makeEaseOut(animation.elastic),
-    //    step: function (d) {
-    //        camera.position.y = d * 1080 - 960;
-    //        camera.position.x = d * 1080 - 960;
-    //    }
-    //});
 
-    threeleds.init();
-
-    threeleds.valueChange = function(val, old){
-        val = (val*(150/255))/100;
-        sphereLights[0].intensity = val;
-        spheres[0].material.emissive.setRGB(val, 0, 0);
-    };
 
     onWindowResize();
 });
